2e1b5cae0a551ff9263c6d343f2a329d35ed4382
lawrence254 <lawrenslava1@gmail.com>
2022-01-21 23:35:34 +0300

    Complet implementation of tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+}
+
+setBestCustomer = () => {
+    bestCustomer = 'not bob'
+}
+
+overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Jonathan'
+changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'Guka'
+}
